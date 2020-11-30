dae999c5e4f1fb0dcd2d039b649c0ea0c3842794
Hannes <hannes6198@gmail.com>
2020-11-30 10:40:15 +0100

    added jump and movement

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,7 +1,64 @@
-namespace DefaultNamespace
+using UnityEngine;
+
+public class Character : IPlayerControl
 {
-    public class Character
+    public void Move(Vector2 moveDirection)
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public void AttackSpecial()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AttackNormal()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AttackCrouch()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AttackForward()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AttackUp()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AttackAir()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Grab()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float GetMovementSpeed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsBusy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Collider2D Get2DCollider()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Sprite GetSprite()
+    {
+        throw new System.NotImplementedException();
     }
 }
