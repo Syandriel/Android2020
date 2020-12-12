86af30d1192194b9bfdb246159ce32983d13e8a1
Awkward <mannsebi97@gmail.com>
2020-12-12 19:36:13 +0100

    Added differentiation between Mobile and PC input
    Mobile and Pc now have different Inputs.
    Mobile has a UI-joystick and UI-buttons
    PC has Keyboard and Controller input

--- Assets/Scripts/Control/Control.cs
+++ Assets/Scripts/Control/Control.cs
@@ -21,107 +21,145 @@ public class Control : MonoBehaviour {
 
     public float clampDistance;
     [Range(0.1f, 1)]
-    public float movementFactor;
+    public float movementFactor = 0.198f;
 
     private bool touchStart = false;
     private Vector2 pointA;
     private Vector2 pointB;
     private Vector2 startA;
 
+    private Text debugText;
+
     // Start is called before the first frame update
     void Start() {
+
+        debugText = GameObject.FindObjectOfType<Text>();
+
+#if (UNITY_ANDROID || UNITY_IOS) //Handy-Seite
         startA = outerJoystick.transform.position;
 
-        //TODO: make platform specific
         attack.onClick.AddListener(Attack);
         special.onClick.AddListener(Special);
         jump.onButtonDown.AddListener(StartJump);
         jump.onButtonUp.AddListener(StopJump);
         grab.onClick.AddListener(Grab);
+#else //PC-Seite
+        attack.gameObject.SetActive(false);
+        special.gameObject.SetActive(false);
+        jump.gameObject.SetActive(false);
+        grab.gameObject.SetActive(false);
+
+        outerJoystick.gameObject.SetActive(false);
+        innerJoystick.gameObject.SetActive(false);
+#endif
+
     }
 
     // Update is called once per frame
     void Update() {
 
-#if (UNITY_ANROID || UNITY_IOS || true)
-        //TODO: Maybe convert to UI
-        //TODO: Refactor to use Input.Touch()
-        //TODO: make Joystick always visible and snap to point of touch
-        if (Input.GetMouseButtonDown(0)) {
-            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
-            if (pointA.x < 0) {
-                outerJoystick.transform.position = pointA;
-                //outerJoystick.GetComponent<SpriteRenderer>().enabled = true;
-                //innerJoystick.GetComponent<SpriteRenderer>().enabled = true;
+#if (UNITY_ANDROID || UNITY_IOS) //Handy-Seite
+
+        int closestFinger = 0;
+        Vector3 touchPosition = Vector3.zero;
+        float distanceToJoystick = Single.MaxValue;
+
+        foreach (Touch touch in Input.touches) {
+            if (touch.phase == TouchPhase.Began) {
+                touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.transform.position.z));
+                if (touch.position.x < Screen.width / 2) {
+                    float distance = Vector3.Distance(touchPosition, outerJoystick.transform.position);
+                    Debug.Log(distance + " " + touch.fingerId);
+                    if (distance < distanceToJoystick) {
+                        closestFinger = touch.fingerId;
+                        distanceToJoystick = distance;
+                    }
+                }
             }
         }
 
-        //TODO: Refactor to use Input.Touch()
-        if(Input.GetMouseButton(0)) {
+        if (Input.touchCount > 0) {
+        TouchPhase touchPhase = Input.GetTouch(closestFinger).phase;
+        if (touchPhase != TouchPhase.Began) {
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.touches[closestFinger].position.x, Input.touches[closestFinger].position.y, Camera.main.transform.position.z));
+            if (pointA.x < 0) {
+                outerJoystick.transform.position = pointA;
+            }
+        } else if (touchPhase == TouchPhase.Moved) {
             touchStart = true;
-            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.touches[closestFinger].position.x, Input.touches[closestFinger].position.y, Camera.main.transform.position.z));
         } else {
             touchStart = false;
             outerJoystick.transform.position = startA;
             innerJoystick.transform.position = startA;
-            //outerJoystick.GetComponent<SpriteRenderer>().enabled = false;
-            //innerJoystick.GetComponent<SpriteRenderer>().enabled = false;
         }
-#else
-        //Input for PC
-        //MoveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    }
+#else //PC-Seite
 
 #endif
     }
 
     // FixedUpdate is called every physics update
     void FixedUpdate() {
-        //TODO: Distinguish between Mobile and PC
-#if (UNITY_ANROID || UNITY_IOS || true)
-        if(touchStart && pointA.x < 0) {
+#if (UNITY_ANDROID || UNITY_IOS) //Handy-Seite
+        if (touchStart && pointA.x < 0) {
             Vector2 offset = pointB - pointA;
             Vector2 direction = Vector2.ClampMagnitude(offset, clampDistance);
 
-            //TODO: instead sendMessage to PlayerController for movement
             MoveCharacter(direction);
 
             innerJoystick.transform.position = pointA + direction;
         } else {
             MoveCharacter(Vector2.zero);
         }
-#else
-        //TODO: PC Update function here
+#else //PC-Seite
+        MoveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+
+        if(Input.GetAxis("Jump") > 0) {
+            StartJump();
+        } else {
+            StopJump();
+        }
+        if(Input.GetAxis("Attack") == 1) {
+            Attack();
+        }
+        if(Input.GetAxis("Special") == 1) {
+            Special();
+        }
+        if(Input.GetAxis("Grab") == 1) {
+            Grab();
+        }
+
 #endif
     }
 
     void MoveCharacter(Vector2 direction) {
-        //player.Translate(direction * Time.deltaTime);
+        Debug.Log(direction.ToString());
         playerController.SendMessage("InputMovement", Vector2.ClampMagnitude(direction, movementFactor));
     }
 
     void Attack() {
-        //TODO: send Message to PlayerController->Attack()
+        Debug.Log("Attack");
         playerController.SendMessage("Attack");
-        Debug.Log("ATTACK");
     }
 
     void Special() {
+        Debug.Log("Special");
         playerController.SendMessage("Special");
-        Debug.Log("SPECIAL");
     }
 
     void StartJump() {
-        Debug.Log("JUMP");
+        Debug.Log("Jump");
         playerController.SendMessage("Jump", value: 1f);
     }
 
     void StopJump() {
-        Debug.Log("Stop Jump");
+        Debug.Log("NoJump");
         playerController.SendMessage("Jump", value: 0f);
     }
 
     void Grab() {
+        Debug.Log("Grab");
         playerController.SendMessage("Grab");
-        Debug.Log("grab");
     }
 }
