8ba08b8599305a7ab6a0522e67dca5064df703b4
Awkward <mannsebi97@gmail.com>
2021-01-26 14:38:30 +0100

    Added DeathScreen

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+
+    public Image image;
+    public float fadeSpeed = 0.1f;
+
+    private float fade = 0;
+
+    public void Start() {
+        
+    }
+
+    public void Update() {
+        image.color = Color.Lerp(Color.black, new Color(0, 0, 0, 0), fade);
+        if(fade > 0) {
+            fade += fadeSpeed;
+        }
+        if(fade <= 0) {
+            fade = 0;
+        }
+    }
+
+    public void TryAgain() {
+        SceneManager.LoadScene(1);
+    }
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,6 +15,7 @@ public class MainMenu: MonoBehaviour
     }
 
     public void HostMultiplayer() {
+        SceneManager.LoadScene("DeathScreen");
         Debug.Log("Hosting Multiplayer...");
     }
 
